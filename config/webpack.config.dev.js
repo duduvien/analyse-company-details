12262d7ec01dec17ea8d917911d5acfe4199cb2a
Kong <mgongzai@gmail.com>
2018-05-17 13:33:49 +0800

    remove postcss next

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -183,18 +183,15 @@ module.exports = {
                   ident: 'postcss',
                   plugins: () => [
                     require('postcss-flexbugs-fixes'),
-                    require('postcss-cssnext')({
-                      browsers: ['last 2 versions']
+                    autoprefixer({
+                      browsers: [
+                        '>1%',
+                        'last 4 versions',
+                        'Firefox ESR',
+                        'not ie < 9', // React doesn't support IE8 anyway
+                      ],
+                      flexbox: 'no-2009',
                     }),
-                    // autoprefixer({
-                    //   browsers: [
-                    //     '>1%',
-                    //     'last 4 versions',
-                    //     'Firefox ESR',
-                    //     'not ie < 9', // React doesn't support IE8 anyway
-                    //   ],
-                    //   flexbox: 'no-2009',
-                    // }),
                   ],
                 },
               },
