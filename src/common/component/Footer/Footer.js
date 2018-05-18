79692db4fcaf8068c605976d3e379e101d99e2a7
Kong <mgongzai@gmail.com>
2018-05-18 10:08:53 +0800

    Link to personalised page

--- src/common/component/Footer/Footer.js
+++ src/common/component/Footer/Footer.js
@@ -1,11 +1,16 @@
-import React, {Component} from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react';
 import styles from './Footer.css';
 
 const Footer = () => (
     <footer className={styles.root}>
         <span>
-            <Link to="/review">Login to your hirer account</Link>&nbsp;to see a personalized dashboard.
+            <a
+                href="http://localhost:8050/show_graph?company=PETRONAS%20Carigali%20Sdn.%20Bhd."
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Login to your hirer account
+            </a>&nbsp;to see a personalized dashboard.
         </span>
     </footer>
 );
